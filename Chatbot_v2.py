9b4cae62103b070f64787d3444cabce0fb819883
iWabitz <shiqizhu321@gmail.com>
2024-08-26 16:37:25 -0700

    Update Chatbot_v2.py

--- Chatbot_v2.py
+++ Chatbot_v2.py
@@ -22,41 +22,45 @@ st.title('Shawn AI 🤖')
 if 'messages' not in st.session_state:
     st.session_state.messages = []
 
-def get_assistant_repsponse(assistant_id, thread_id, user_input):
+def get_assistant_response(assistant_id, thread_id, user_input):
     try:
         # Add the user's message to the thread
         client.beta.threads.messages.create(
-                thread_id=thread_id,
-                role='user',
-                content=user_input
-            )
-        run = client.beta.threads.messages.runs(
+            thread_id=thread_id,
+            role="user",
+            content=user_input
+        )
+        run = client.beta.threads.runs.create(
                 thread_id=thread_id,
                 assistant_id=assistant_id
-            )
+        )
 
         # wait for the run to complete
         while True:
             run_status = client.beta.threads.runs.retrieve(thread_id=thread_id, run_id = run.id)
 
-            if run_status.status=='completed':
+            if run_status.status == 'completed':
                 break
             time.sleep(1)
 
         # Retrieve the Assistant's message
         messages = client.beta.threads.messages.list(thread_id=thread_id)
 
-        return messages.data[0].text.value
+        return messages.data[0].content[0].text.value
     except Exception as e:
-        st.error(f'Error getting assistant response: {str(e)}')
-        return 'I\m sorry but that did not work :('
+        st.error(f"Error getting assistant response: {str(e)}")
+        return "I\m sorry but that did not work :("
+
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
 
 prompt = st.chat_input("Ask me anything!")
 if prompt:
     st.session_state.messages.append({"role": "user", "content": prompt})
     with st.chat_message("user"):
         st.markdown(prompt)
-    
+
     with st.chat_message("assistant"):
         message_placeholder = st.empty()
         full_response = get_assistant_response(
